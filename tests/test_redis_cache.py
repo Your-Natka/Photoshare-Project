14ddb3f9309db6b1c1e1306c6c82e9a02d2bd41b
Olena Yakovchuk <elenaygolkova1989@gmail.com>
2025-11-27 18:08:01 +0100

    redis cache test

--- tests/test_redis_cache.py
+++ tests/test_redis_cache.py
@@ -1,6 +1,34 @@
 import pytest
 from unittest.mock import AsyncMock
 from app.cache import redis_cache
+from unittest.mock import MagicMock
+
+
+@pytest.fixture(autouse=True)
+def fake_redis(monkeypatch):
+    class FakeRedis:
+        storage = {}
+
+        async def set(self, key, value):
+            self.storage[key] = value
+
+        async def get(self, key):
+            value = self.storage.get(key, None)
+            if value is not None:
+                return value.encode()   
+            return None
+
+        async def ping(self):
+            return True
+
+    fake = FakeRedis()
+
+    monkeypatch.setattr(redis_cache, "set", fake.set)
+    monkeypatch.setattr(redis_cache, "get", fake.get)
+    monkeypatch.setattr(redis_cache, "ping", fake.ping)
+
+    return fake
+
 
 @pytest.mark.asyncio
 async def test_redis_ping(monkeypatch):
